ce8a18b6a373945ab53761e7c530c7069be488cb
Lionel Francis <lezardvaldth@gmail.com>
2022-07-07 13:11:40 -0500

    update complaint title to test the pipeline again

--- app/Http/Resources/CoordinatorResource.php
+++ app/Http/Resources/CoordinatorResource.php
@@ -15,6 +15,7 @@ class CoordinatorResource extends JsonResource
     public function toArray($request)
     {
         return [
+            
             'id' => $this->id,
             'name' => $this->firstname.' '.$this->lastname,
             'email' => $this->email,
