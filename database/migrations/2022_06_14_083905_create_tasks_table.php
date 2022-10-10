4be51726ca8b17ed527f3f25efc29bec0ebeca36
sparkinsanchor <sparkins@anchorgroupcorp.com>
2022-10-10 11:52:05 -0500

    Update to backed to facilitate new changes to sub task ans task module

--- database/migrations/2022_06_14_083905_create_tasks_table.php
+++ database/migrations/2022_06_14_083905_create_tasks_table.php
@@ -17,11 +17,33 @@ class CreateTasksTable extends Migration
             $table->id();
             $table->string('uid');
             $table->string('title');
+
             $table->string('levels');
             $table->string('frequency');
+            $table->string('module');
+            $table->string('category');
             $table->string('user_id');
-            $table->date('date');
-            $table->time('time')->nullable();
+            $table->date('schedule_date');
+            $table->time('schedule_time')->nullable();
+
+            $table->string('assigned_to');
+            $table->string('assigned_type');
+            $table->string('reassign_reason')->nullable();
+            $table->date('reassign_date')->nullable();
+            $table->string('original_assigned')->nullable();
+            $table->string('original_assigned_type')->nullable();
+
+            $table->string('escalation_reason')->nullable();
+            //$table->string('escalated_by')->nullable();
+            //$table->date('escalation_date')->nullable();
+            //$table->date('review_date')->nullable();
+
+            $table->boolean('isClompleted')->default(0);
+            $table->boolean('isReAssigned')->default(0);
+            //$table->boolean('isRedo')->default(0);
+            //$table->boolean('isEscalation')->default(0);
+            //$table->boolean('isReviewed')->default(0);
+            $table->boolean('isDeleted')->default(0);;
             $table->timestamps();
         });
     }
