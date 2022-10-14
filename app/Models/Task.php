6743f31cba886f4c5dc558c9bb4dded5bb0f4214
sparkinsanchor <sparkins@anchorgroupcorp.com>
2022-10-14 10:07:50 -0500

    update Task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -12,11 +12,36 @@ class Task extends Model
     use HasFactory,  LogsActivity;
 
     protected $fillable = ['title', 'uid', 'frequency', 'levels', 'date', 'time'];
-    
+    protected $appends = ["role_name", "task_category_title"];
+
     public function getActivitylogOptions(): LogOptions
     {
         return LogOptions::defaults()
         ->logOnly(['title', 'uid', 'frequency', 'levels', 'date', 'time']);
         // Chain fluent methods for configuration options
+    }
+
+    public function taskCat(){
+
+        return $this->belongsTo(TaskCategory::class, 'category');
+
+    }
+
+    public function taskAssignedTo(){
+
+        return $this->belongsTo(Role::class, 'assigned_to');
+
+    }
+
+    public function getRoleNameAttribute(){
+
+        return $this->taskAssignedTo()->first()->name;
+
+    }
+
+    public function getTaskCategoryTitleAttribute(){
+
+        return $this->taskCat()->first()->title;
+
     }
 }
